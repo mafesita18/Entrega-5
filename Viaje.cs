4094e6637844ddc7ad718783d5565e0cc37fbc49
mafesita18 <109915580+mafesita18@users.noreply.github.com>
2022-11-04 18:59:46 -0500

    Initial commit

---
+++ Viaje.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metroplus
+{
+    public class Viaje
+    {
+        //Atributos
+        private string ruta;
+        private int cantidadPasajeros;
+
+        //Constructor
+        public Viaje()
+        {
+            ruta = "";
+            cantidadPasajeros = 0;
+        }
+
+        //Propiedades
+        public string Ruta
+        {
+            get { return ruta; }
+            set { ruta = value; }
+        }
+
+        public int CantidadPasajeros
+        {
+            get { return cantidadPasajeros; }
+            set { cantidadPasajeros = value; }
+        }
+    }
+}
